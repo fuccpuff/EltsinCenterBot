1a05bee40cac99d4eefd6bd718b3d2201548b528
syviline <dovase71@gmail.com>
2024-11-05 15:13:55 +0500

    Hardcode tokens

--- config.py
+++ config.py
@@ -4,8 +4,8 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-TOKEN = os.getenv('TELEGRAM_BOT_TOKEN')
-YANDEX_MAPS_API_KEY = os.getenv('YANDEX_MAPS_API_KEY')
+TOKEN = 'API TOKEN HERE'
+YANDEX_MAPS_API_KEY = 'API TOKEN HERE'
 DATABASE_NAME = 'bot.db'
 ADMIN_USER_ID = int(os.getenv('ADMIN_USER_ID'))
 
